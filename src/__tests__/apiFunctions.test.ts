ea9740ebc40f0ce540fbeda033a3eb03468f856c
gechdejenb <gechdejenb@gmail.com>
2024-12-29 14:02:35 +0300

    remove unused libraries

--- src/__tests__/apiFunctions.test.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:43f16a7a8be89d20582f3be4e1ac31ccbdb0d3a69462bce03caac97ebdd112a7
-size 4722
--- src/services/apiFunctions.ts
+++ src/services/apiFunctions.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1b7263ab237a14d5c4f9350c6f8c6a31ca562a0b7a50e631eea69266f09d688
+oid sha256:a74dbfd23bd0ec8191640e85f1525bdb776c98b5a1bf8b23daae7dfe2a99f8e3
 size 5162
