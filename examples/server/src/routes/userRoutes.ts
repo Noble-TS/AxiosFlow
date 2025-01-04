c68f3152ee96491f93a69764842a7ad09601297d
gechdejenb <gechdejenb@gmail.com>
2025-01-04 11:02:19 +0300

    update middleware usage

--- examples/server/src/routes/userRoutes.ts
+++ examples/server/src/routes/userRoutes.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:577e7cd1650cd7234e4408e71704a91631b5d0cb89e08326559f2a1f3eb804a2
-size 1394
+oid sha256:d8f101bbf545fae2c7ba261bd5e7a3b59d90b556aba138e38c12d4c69f45ea45
+size 1330
